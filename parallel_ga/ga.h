9a96f12c3da71924943bc8e7aea76469d5d24ce5
Raymond Chee <rjchee@gmail.com>
2018-12-10 18:54:00 -0500

    Add parallel version of code

---
+++ parallel_ga/ga.h
@@ -0,0 +1,24 @@
+#ifndef __GA_H__
+#define __GA_H__
+
+typedef struct {
+	char val;
+} gene_t;
+
+typedef struct {
+	int numOfGenes;
+	int fitness;
+        int geneIdx;
+} chromosome_t;
+
+typedef struct {
+	double mutationProb;
+	int numChromosomes;
+        int numGenes;
+        int totalFitness;
+	chromosome_t *chromosomes;
+        gene_t *genes;
+} population_t;
+
+
+#endif
